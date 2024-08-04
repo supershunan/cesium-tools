76c49dc8910fe0979282071860e5291ec2fa5085
supershunan <1552736550@qq.com>
2024-08-05 00:26:31 +0800

    #chore: npm包配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,31 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@src': path.resolve(__dirname, './src'),
-            '@tools': path.resolve(__dirname, './src/tools'),
-            '@components': path.resolve(__dirname, './src/components')
-          }
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@tools': path.resolve(__dirname, './src/tools'),
+      '@components': path.resolve(__dirname, './src/components'),
     },
-    build: {
-        rollupOptions: {
-            input: path.resolve(__dirname, './src/index.ts'),
-            output: {
-            file: path.resolve(__dirname, './dist/cesium-tools.min.js'),
-            format: 'umd',
-            name: 'CesiumToolsFXT',
-            globals: {
-                'cesium': 'Cesium',
-                'react': 'React'
-            }
-            }
-        }
-    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/index.ts'),
+      name: 'CesiumToolsFXT',
+      fileName: (format) => {return `cesium-tools.${format}.js`;},
+    },
+    rollupOptions: {
+      output: {
+        dir: path.resolve(__dirname, './dist'),
+        format: 'umd',
+        entryFileNames: 'cesium-tools.min.js',
+        name: 'CesiumToolsFXT',
+        globals: {
+          cesium: 'Cesium',
+          react: 'React',
+        },
+      },
+    },
+  },
 });

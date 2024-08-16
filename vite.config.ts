121fc177b7dd9c216149e9c754e46bec0123bf69
wangkun <wangkun@mp.com>
2024-08-16 16:05:06 +0800

    #chore: 发包完成

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,40 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@src': path.resolve(__dirname, './src'),
-      '@tools': path.resolve(__dirname, './src/tools'),
-      '@components': path.resolve(__dirname, './src/components'),
-    },
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, './src/index.ts'),
-      name: 'CesiumToolsFXT',
-      fileName: (format) => {return `cesium-tools.${format}.js`;},
-    },
-    rollupOptions: {
-      output: {
-        dir: path.resolve(__dirname, './dist'),
-        format: 'umd',
-        entryFileNames: 'cesium-tools.min.js',
-        name: 'CesiumToolsFXT',
-        globals: {
-          cesium: 'Cesium',
-          react: 'React',
+    plugins: [
+        react(),
+    ],
+    resolve: {
+        alias: {
+            '@src': path.resolve(__dirname, './src'),
+            '@tools': path.resolve(__dirname, './src/tools'),
+            '@components': path.resolve(__dirname, './src/components'),
         },
-      },
     },
-  },
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, './src/index.ts'),
+            name: 'CesiumToolsFXT',
+            formats: ['es', 'umd'],
+            fileName: (format) => {return `cesium-tools.${format}.js`;},
+        },
+        rollupOptions: {
+            external: [
+                'react',
+                'cesium',
+                '@turf/turf',
+            ],
+            output: {
+                globals: {
+                    cesium: 'Cesium',
+                    react: 'React',
+                    '@turf/turf': 'turf',
+                },
+            },
+        },
+        // sourcemap: true,
+        copyPublicDir: false,
+        minify: 'esbuild'
+    },
 });
